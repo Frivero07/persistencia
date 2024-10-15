c16761d8b68def209fc098f76507f43ee3497157
Frivero07 <141739171+Frivero07@users.noreply.github.com>
2024-10-15 17:54:15 -0300

    Add files via upload

---
+++ ViajeESpacial/Models/Exploracion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViajeESpacial.Models
+{
+    public class Exploracion : Mision
+    {
+        public Exploracion(string nombre, Destinos destinos, int astronautas) : base(nombre, destinos, astronautas) { }
+        
+        public override double CalcularDuracion()
+        {
+            return Astronautas * 1.5 + (int)Destinos * 2;
+        } 
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}, {Nombre}, {Destinos}, {Astronautas}";
+        }
+    }
+}
