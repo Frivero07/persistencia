e56b4c9b70e6a719f506408ce619747c3dcfc067
Frivero07 <141739171+Frivero07@users.noreply.github.com>
2024-10-17 15:17:22 -0300

    Add files via upload

---
+++ ViajeESpacial/Models/Colonizacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViajeESpacial.Models
+{
+     public class Colonizacion : Mision
+     {
+        public int Colonos {  get; private set; }
+        public Colonizacion(string nombre, Destinos destinos, int astronautas,int colonos) : base(nombre, destinos, astronautas) { }
+        public override double CalcularDuracion()
+        {
+            return Astronautas * 2.5 + (int)Destinos * 3.5;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}, {Nombre}, {Destinos}, {Astronautas}, {Colonos}";
+        }
+    }
+}
---
+++ ViajeESpacial/Models/Destinos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViajeESpacial.Models
+{
+    public enum Destinos
+    {
+        Marte,
+        Luna,
+        EstacionEspacial,
+        Asteroide,
+        Titan
+    }
+}
